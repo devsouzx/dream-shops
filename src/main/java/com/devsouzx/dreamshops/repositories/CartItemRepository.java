fa3f1e1d707f40ad5868d69426a35db720f5e30e
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-12 19:16:49 -0300

    feat: implementing the cartItem service

---
+++ src/main/java/com/devsouzx/dreamshops/repositories/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.devsouzx.dreamshops.repositories;
+
+import com.devsouzx.dreamshops.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteAllByCartId(Long id);
+}
