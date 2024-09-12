33e025ffd3ee3002d01dcf785a529cc6e808c860
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-12 19:04:00 -0300

    feat: implementing the cart service

---
+++ src/main/java/com/devsouzx/dreamshops/services/cart/CartService.java
@@ -0,0 +1,42 @@
+package com.devsouzx.dreamshops.services.cart;
+
+import com.devsouzx.dreamshops.exceptions.ResourceNotFoundException;
+import com.devsouzx.dreamshops.model.Cart;
+import com.devsouzx.dreamshops.repositories.CartItemRepository;
+import com.devsouzx.dreamshops.repositories.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartService implements ICartService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    @Override
+    public Cart getCart(Long id) {
+        Cart cart = cartRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        return cartRepository.save(cart);
+    }
+
+    @Transactional
+    @Override
+    public void clearCart(Long id) {
+        Cart cart = getCart(id);
+        cartItemRepository.deleteAllByCartId(id);
+        cart.getItems().clear();
+        cartRepository.deleteById(id);
+    }
+
+    @Override
+    public BigDecimal getTotalPrice(Long id) {
+        Cart cart = getCart(id);
+        return cart.getTotalAmount();
+    }
+}
---
+++ src/main/java/com/devsouzx/dreamshops/services/cart/ICartService.java
@@ -0,0 +1,11 @@
+package com.devsouzx.dreamshops.services.cart;
+
+import com.devsouzx.dreamshops.model.Cart;
+
+import java.math.BigDecimal;
+
+public interface ICartService {
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
+}
