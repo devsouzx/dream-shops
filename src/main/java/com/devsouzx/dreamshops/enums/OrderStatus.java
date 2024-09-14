9f34710c5bba3c88ed65c9dc696ea08ef95d1d72
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-14 16:22:34 -0300

    feat: creating the order entities

---
+++ src/main/java/com/devsouzx/dreamshops/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.devsouzx.dreamshops.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/devsouzx/dreamshops/model/Order.java
@@ -0,0 +1,30 @@
+package com.devsouzx.dreamshops.model;
+
+import com.devsouzx.dreamshops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+}
