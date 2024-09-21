cb11539e39b9a8e014986daeeb197f98e5d6fb72
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-21 18:11:28 -0300

    feat: implementing the login api

---
+++ src/main/java/com/devsouzx/dreamshops/requests/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.devsouzx.dreamshops.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Invalid credentials")
+    private String email;
+
+    @NotBlank(message = "Invalid credentials")
+    private String password;
+}
